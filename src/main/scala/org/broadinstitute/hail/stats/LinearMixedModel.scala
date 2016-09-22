e717fccc98fbebfd4d3b63ea4df2b0748c1a86eb
jbloom22 <jbloom@broadinstitute.org>
2016-09-22 16:38:38 -0400

    working on spark svd version, added genAndFitLMMTestIndexedRowMatrix

--- src/main/scala/org/broadinstitute/hail/stats/LinearMixedModel.scala
+++ src/main/scala/org/broadinstitute/hail/stats/LinearMixedModel.scala
@@ -2,15 +2,65 @@ package org.broadinstitute.hail.stats
 
 import breeze.linalg._
 import org.apache.commons.math3.special.Gamma
-import breeze.numerics._
+import org.apache.spark.mllib.linalg.SingularValueDecomposition
+import org.apache.spark.mllib.linalg.distributed.IndexedRowMatrix
+import org.apache.spark.rdd.RDD
+import org.broadinstitute.hail.methods.{ToIndexedRowMatrix, ToStandardizedIndexedRowMatrix}
+import org.broadinstitute.hail.variant.{Variant, VariantDataset}
+import org.broadinstitute.hail.utils._
 
 
+object LMM {
+  def applyVds(vds: VariantDataset,
+    filtTest: Variant => Boolean,
+    filtGRM: Variant => Boolean,
+    C: DenseMatrix[Double],
+    y: DenseVector[Double],
+    k: Int,
+    optDelta: Option[Double] = None): LMMResult = {
+
+    val Wt = ToStandardizedIndexedRowMatrix(vds.filterVariants((v, va, gs) => filtGRM(v)))._2 // W is samples by variants, Wt is variants by samples
+    val (variants, genotypes) = ToIndexedRowMatrix(vds.filterVariants((v, va, gs) => filtTest(v)))
+
+    LMM(Wt, variants, genotypes, C, y, k, optDelta)
+  }
+
+  def apply(Wt: IndexedRowMatrix,
+    variants: Array[Variant],
+    genotypes: IndexedRowMatrix,
+    C: DenseMatrix[Double],
+    y: DenseVector[Double],
+    k: Int,
+    optDelta: Option[Double] = None): LMMResult = {
+
+    require(k <= Wt.numRows())
+    require(k <= Wt.numCols())
+
+    val svd: SingularValueDecomposition[IndexedRowMatrix, org.apache.spark.mllib.linalg.Matrix] = Wt.computeSVD(k)
+
+    val U = svd.V // W = svd.V * svd.s * svd.U.t
+    val s = toBDenseVector(svd.s.toDense)
+    val SB =  s :* s // K = U * (svd.s * svd.s) * V.t
+    val UB = new DenseMatrix[Double](svd.V.numRows, svd.V.numCols, svd.V.toArray)
+
+    val diagLMM = DiagLMM(UB.t * C, UB.t * y, SB, optDelta)
+
+    val diagLMMBc = genotypes.rows.sparkContext.broadcast(diagLMM)
+
+    // FIXME: variant names are already local, we shouldn't be gathering them all, right?!
+
+    val lmmResult = genotypes.multiply(U).rows.map(r =>
+      (variants(r.index.toInt), diagLMMBc.value.likelihoodRatioTest(toBDenseVector(r.vector.toDense))))
+
+    LMMResult(diagLMM, lmmResult)
+  }
+}
 
 object DiagLMM {
   def apply(C: DenseMatrix[Double], y: DenseVector[Double], S: DenseVector[Double], optDelta: Option[Double] = None): DiagLMM = {
     require(C.rows == y.length)
 
-    val delta = optDelta.getOrElse(findDelta(C, y, S))
+    val delta = optDelta.getOrElse(deltaGridVals(C, y, S).minBy(_._2)._1)
 
     val n = y.length
     val D = S + delta
@@ -21,10 +71,11 @@ object DiagLMM {
     val b = xdx \ xdy
     val s2 = (ydy - (xdy dot b)) / n
 
-    DiagLMM(C, y, dy, ydy, b, s2, log(s2), delta, D.map(1 / _))
+    DiagLMM(C, y, dy, ydy, b, s2, math.log(s2), delta, D.map(1 / _))
   }
 
-  def findDelta(C: DenseMatrix[Double], y: DenseVector[Double], S: DenseVector[Double]): Double = {
+  def deltaGridVals(C: DenseMatrix[Double], y: DenseVector[Double], S: DenseVector[Double]): IndexedSeq[(Double, Double)] = {
+
     val logmin = -10.0
     val logmax = 10.0
     val logres = 0.1
@@ -43,15 +94,13 @@ object DiagLMM {
       val b = xdx \ xdy
       val s2 = (ydy - (xdy dot b)) / n
 
-      sum(log(D)) + n * math.log(s2)
+      sum(breeze.numerics.log(D)) + n * math.log(s2)
     }
 
-    val minIndex = grid.map(negLogLkhd).zipWithIndex.minBy(_._1)._2
-
-    grid(minIndex)
+    grid.map(delta => (delta, negLogLkhd(delta)))
   }
 
-  // can removed once logreg is in
+  // remove once logreg is in
   def chiSquaredTail(df: Double, x: Double) = Gamma.regularizedGammaQ(df / 2, x / 2)
 }
 
@@ -66,7 +115,7 @@ case class DiagLMM(
   delta: Double,
   invD: DenseVector[Double]) {
 
-  def likelihoodRatioTest(x: DenseVector[Double]): LMMStats = {
+  def likelihoodRatioTest(x: DenseVector[Double]): LMMStat = {
     require(x.length == y.length)
 
     val n = x.length
@@ -77,11 +126,13 @@ case class DiagLMM(
 
     val b = xdx \ xdy
     val s2 = (ydy - (xdy dot b)) / n
-    val chi2 = n * (logNullS2 - log(s2))
+    val chi2 = n * (logNullS2 - math.log(s2))
     val p = DiagLMM.chiSquaredTail(1, chi2)
 
-    LMMStats(b, s2, chi2, p)
+    LMMStat(b, s2, chi2, p)
   }
 }
 
-case class LMMStats(b: DenseVector[Double], s2: Double, chi2: Double, p: Double)
+case class LMMStat(b: DenseVector[Double], s2: Double, chi2: Double, p: Double)
+
+case class LMMResult(diagLMM: DiagLMM, stats: RDD[(Variant, LMMStat)])
