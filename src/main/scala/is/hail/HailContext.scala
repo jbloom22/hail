66cd7ffc130771ceda2867c32b85d3ed25503421
jbloom22 <jbloom@broadinstitute.org>
2017-10-06 16:15:06 -0400

    added serializer check to HailContext

--- src/main/scala/is/hail/HailContext.scala
+++ src/main/scala/is/hail/HailContext.scala
@@ -108,6 +108,14 @@ object HailContext {
         problems += s"Invalid config parameter '$k': too small. Found $param, require at least 50G"
     }
 
+    val serializer = conf.get("spark.serializer")
+    val kryoSerializer = "org.apache.spark.serializer.KryoSerializer"
+    if (serializer != kryoSerializer)
+      problems += s"Invalid configuration property spark.serializer: required $kryoSerializer.  Found: $serializer."
+
+    if (!conf.getOption("spark.kryo.registrator").contains("is.hail.kryo.HailKryoRegistrator"))
+      problems += s"Invalid config parameter: spark.kryo.registrator must be set to is.hail.kryo.HailKryoRegistrator"    
+    
     if (problems.nonEmpty)
       fatal(
         s"""Found problems with SparkContext configuration:
