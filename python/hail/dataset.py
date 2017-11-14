e621e068ab87d491ef918d217b982ec9b90b3bc8
jbloom22 <jbloom@broadinstitute.org>
2017-11-14 17:56:31 -0500

    updated doc

--- python/hail/dataset.py
+++ python/hail/dataset.py
@@ -1615,11 +1615,14 @@ class VariantDataset(HistoryMixin):
         Sets and Arrays are output with the same comma-separated format.
         Lastly, fields of type Boolean are allowed in ``va.info`` to generate INFO fields of type Flag.
 
+        Hail also exports the name of the genome reference (e.g. ``GRCh37``), as well as contig names and lengths,
+        as VCF header lines.
+
         Consider the workflow of importing a VCF to VDS and immediately exporting VDS to VCF:
 
         >>> vds.export_vcf('output/example_out.vcf')
 
-        The *example_out.vcf* header will contain the FORMAT, FILTER, and INFO lines present in *example.vcf*. However, it will *not* contain CONTIG lines or lines added by external tools (such as bcftools and GATK) unless they are explicitly inserted using the ``append_to_header`` option.
+        The *example_out.vcf* header will contain the FORMAT, FILTER, and INFO lines present in *example.vcf*. However, it will *not* contain lines added by external tools (such as bcftools and GATK) unless they are explicitly inserted using the ``append_to_header`` option.
 
         .. caution::
 
